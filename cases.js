d237244faa5cd1a9ea112324f652d3f5e32c854c
Al-mohamady law group <al.mohamady2026@gmail.com>
2026-01-18 14:57:28 +0200

    Add files via upload

---
+++ cases.js
@@ -0,0 +1,11 @@
+const casesData = [
+  {
+    fullName: "محمد عبدالنبي محمد علي",
+    nationalID: "01029207323",
+    caseNumber: "101",
+    caseYear: "2025",
+    caseType: "جنايات",
+    lastAction: "للحكم",
+    lastUpdate: "2025-12-20"
+  }
+];
